a607c8294d949f706306125b7dccf20946fc30e3
muzzamiah <muzzamiah123@gmail.com>
2024-05-07 14:52:24 +0100

    Practical 7 - unifnished

--- Testing1/tstAddress.cs/tstAddress.cs
+++ Testing1/tstAddress.cs/tstAddress.cs
@@ -4,13 +4,34 @@ using System;
 namespace tstAddress.cs
 {
     [TestClass]
+
     public class tstAddress
+    
     {
         [TestMethod]
+    
         public void InstanceOK()
+        
         {
+        
             clsAddress AnAddress = new clsAddress();
+            
             Assert.IsNotNull(AnAddress);
         }
+
+        [TestMethod]
+
+        public void ActivePropertyOK()
+        
+            clsAddress AnAddress = new clsAddress();
+        
+            Boolean TestData = true;
+            
+            AnAddress.Active = TestData;
+            
+            Assert.AreEqual(AnAddress.Active, TestData);
+
     }
 }
+
+]
