6ad91c71f1bc083dc968547aa0599de05909ae76
muzzamiah <muzzamiah123@gmail.com>
2024-05-20 17:11:31 +0100

    Practical 7 - no issues

--- Testing1/Practical7.cs
+++ Testing1/Practical7.cs
@@ -8,62 +8,112 @@ namespace Testing1
     public class tstAddress
     {
         [TestMethod]
-        public void InstanceOK()
+        public void StaffName()
         {
             //create an instance of the class we want to create
-            clsAddress AnAddress = new clsAddress();
+            StaffName AName = new StaffName();
             //test to see that it exists
-            Assert.IsNotNull(AnAddress);
+            Assert.IsNotNull(AName);
         }
         [TestMethod]
-        public void ActivePropertyOK()
+        public void StaffID()
         {
             //create an instance of the class we want to create
-            clsAddress AnAddress = new clsAddress();
+            StaffName AName = new StaffName();
             //create some test data to assign to the property
-            Boolean TestData = true;
+            Int32 TestData = 1;
             //assign the data to the property
-            AnAddress.Active = TestData;
+            AName.StaffId = TestData;
             //test to see that the two values are the same
-            Assert.AreEqual(AnAddress.Active, TestData);
+            Assert.AreEqual(AName.StaffId, TestData);
         }
 
         [TestMethod]
-        public void PostCodePropertyOK()
+        public void StaffDOB()
         {
-            //create an instance of the class we want to create
-            clsAddress AnAddress = new clsAddress();
-            //create some test data to assign to the property
-            string TestData = "LE1 4AB";
-            //assign the data to the property
-            AnAddress.PostCode = TestData;
-            //test to see that the two values are the same
-            Assert.AreEqual(AnAddress.PostCode, TestData);
+            // Create an instance of the class we want to create
+            StaffName AName = new StaffName();
+
+            // Create some test data to assign to the property
+            DateTime TestData = DateTime.Today.AddYears(-70); // Example date 70 years in the past
+
+            // Assign the data to the property
+            AName.DOB = TestData;
+
+            // Test to see that the two values are the same
+            Assert.AreEqual(TestData, AName.DOB);
         }
         [TestMethod]
-        public void StreetPropertyOK()
+        public void StaffStreet()
         {
             //create an instance of the class we want to create
-            clsAddress AnAddress = new clsAddress();
+            StaffName AName = new StaffName();
             //create some test data to assign to the property
             string TestData = "Some Street";
             //assign the data to the property
-            AnAddress.Street = TestData;
+            AName.Street = TestData;
             //test to see that the two values are the same
-            Assert.AreEqual(AnAddress.Street, TestData);
+            Assert.AreEqual(AName.Street, TestData);
         }
 
         [TestMethod]
-        public void TownPropertyOK()
+        public void StaffPostCode()
         {
             //create an instance of the class we want to create
-            clsAddress AnAddress = new clsAddress();
+            StaffName AName = new StaffName();
             //create some test data to assign to the property
-            string TestData = "Leicester";
+            string TestData = "LE2 7EF ";
             //assign the data to the property
-            AnAddress.Town = TestData;
+            AName.PostCode = TestData;
             //test to see that the two values are the same
-            Assert.AreEqual(AnAddress.Town, TestData);
+            Assert.AreEqual(AName.PostCode, TestData);
+        }
+
+        [TestMethod]
+        public void StaffRole()
+        {
+            // Create an instance of the class we want to create
+            StaffName AName = new StaffName();
+
+            // Create some test data to assign to the property
+            string TestData = "Manager";
+
+            // Assign the data to the property
+            AName.Role = TestData;
+
+            // Test to see that the two values are the same
+            Assert.AreEqual(TestData, AName.Role);
+        }
+        [TestMethod]
+        public void DateAdded()
+        {
+            StaffName AName = new StaffName();
+            //create an instance of the class we want to create
+            DateTime.Today.AddYears(-70);
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Today.AddYears(-5);
+            //assign the data to the property
+            AName.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AName.DateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void Attendance()
+        {
+            // Create an instance of the class we want to create
+            StaffName AName = new StaffName();
+
+            // Create some test data to assign to the property
+            bool TestData = true;
+
+            // Assign the data to the property
+            AName.Attendance = TestData;
+
+            // Test to see that the two values are the same
+            Assert.AreEqual(TestData, AName.Attendance);
+
+
         }
 
     }
---
+++ Testing1/StaffName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Testing1
+{
+    internal class StaffName
+    {
+        public int StaffId { get; internal set; }
+        public DateTime DOB { get; internal set; }
+        public string Street { get; internal set; }
+        public string PostCode { get; internal set; }
+        public string Role { get; internal set; }
+        public DateTime DateAdded { get; internal set; }
+        public bool Attendance { get; internal set; }
+    }
+}
