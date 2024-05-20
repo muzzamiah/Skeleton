60b54b004c94541a386393c5ce15140a72e6c599
muzzamiah <muzzamiah123@gmail.com>
2024-05-20 16:27:06 +0100

    Practical 7 complete

---
+++ Testing1/Practical7.cs
@@ -0,0 +1,71 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstAddress
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsAddress AnAddress = new clsAddress();
+            //test to see that it exists
+            Assert.IsNotNull(AnAddress);
+        }
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsAddress AnAddress = new clsAddress();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AnAddress.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnAddress.Active, TestData);
+        }
+
+        [TestMethod]
+        public void PostCodePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsAddress AnAddress = new clsAddress();
+            //create some test data to assign to the property
+            string TestData = "LE1 4AB";
+            //assign the data to the property
+            AnAddress.PostCode = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnAddress.PostCode, TestData);
+        }
+        [TestMethod]
+        public void StreetPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsAddress AnAddress = new clsAddress();
+            //create some test data to assign to the property
+            string TestData = "Some Street";
+            //assign the data to the property
+            AnAddress.Street = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnAddress.Street, TestData);
+        }
+
+        [TestMethod]
+        public void TownPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsAddress AnAddress = new clsAddress();
+            //create some test data to assign to the property
+            string TestData = "Leicester";
+            //assign the data to the property
+            AnAddress.Town = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnAddress.Town, TestData);
+        }
+
+    }
+
+}
---
+++ Testing1/clsAddress.cs
@@ -0,0 +1,10 @@
+﻿namespace Testing1
+{
+    internal class clsAddress
+    {
+        public bool Active { get; set; }
+        public string PostCode { get; internal set; }
+        public string Street { get; internal set; }
+        public string Town { get; internal set; }
+    }
+}
