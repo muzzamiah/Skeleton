f41c05bcc5d5f5b98b4e19c5df16d07c173310f7
isaaq3012 <P2723318@my365.dmu.ac.uk>
2024-05-28 17:49:42 +0100

    P9A complete

--- ClassLibrary/clsAddress.cs
+++ ClassLibrary/clsAddress.cs
@@ -11,6 +11,48 @@ namespace ClassLibrary
         public int Quantity { get; set; }
         public int OrderNo { get; set; }
         public decimal Price { get; set; }
-        public string Name { get; set; }
+        public string OrderAddress { get; set; }
+
+        public bool Find(int addressId)
+        {
+            //always return true
+            return true;
+        }
+        //private data member for the address id property
+        private Int32 mAddressId;
+        //addressId public property
+        public int AddressId
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mAddressId;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mAddressId = value;
+            }
+
+        }
+
+        //private data member for the date added property
+        private DateTime mDateAdded;
+        private DateTime mOrderDateFound;
+
+        //date added public property
+        public DateTime OrderDateFound
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mOrderDateFound;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mDateAdded = value;
+            }
+        }
     }
-}
+    
